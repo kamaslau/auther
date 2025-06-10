80b1473f9a38565fccf80da59facc70d273e8828
Kamas Lau <kamaslau@outlook.com>
2025-06-11 02:14:30 +0800

    fix: repair eslint config

--- eslint.config.js
+++ eslint.config.js
@@ -1,20 +1,28 @@
 import globals from "globals";
 import pluginJs from "@eslint/js";
-import tseslint from "typescript-eslint";
+import tseslint from "@typescript-eslint/eslint-plugin";
+import tsParser from "@typescript-eslint/parser";
 
 export default [
+  pluginJs.configs.recommended,
   {
+    files: ["src/**/*.ts", "src/**/*.js"],
     languageOptions: {
       ecmaVersion: "latest",
       sourceType: "module",
       globals: { ...globals.node },
+      parser: tsParser,
+      parserOptions: {
+        project: "./tsconfig.json",
+        sourceType: "module",
+      },
     },
-  },
-  pluginJs.configs.recommended,
-  ...tseslint.configs.recommended,
-  {
+    plugins: {
+      "@typescript-eslint": tseslint,
+    },
+    ...tseslint.configs.flatRecommended,
     rules: {
-      "no-undef": "warn",
+      "no-undef": "off",
       "@typescript-eslint/no-unused-vars": "warn",
       "@typescript-eslint/no-explicit-any": "warn",
     },
